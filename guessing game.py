846313a7d7da670dfacb9e1d25a0395f4114546c
bradenluvzlax <18spd01@westottawa.net>
2017-05-02 11:11:10 -0400

    Change so it does not say 1 guesses left anymore

--- guessing game.py
+++ guessing game.py
@@ -3,21 +3,27 @@ import random
 y = random.randint(1,100) 
 tries = 0
 while tries < 5:
-    x = int(input("Guess a number 1-100: "))
+    x = int(input("Guess a whole number 1-100: "))
     if x < 1 or x > 100:
         print ("Hey! that is not even between 1 and 100!")
         tries = tries + 1
     elif x == y:
-        print ("wow first try!!! YOU WIN")
+        print ("nice guess!!! YOU WIN")
         break
     elif x < y:
         print ("Too low!")
         tries = tries + 1
-        print (5 - tries, "guess(es) left")
+        if tries >= 4:
+            print (5 - tries, "guess left")
+        else:
+            print (5 - tries, "guesses left")
     elif x > y:
         print ("Too high!")
         tries = tries + 1
-        print (5 - tries, "guess(es) left")
+        if tries >= 4:
+            print (5 - tries, "guess left")
+        else:
+            print (5 - tries, "guesses left")
 if tries > 5:
     print ("You Lose!")
 

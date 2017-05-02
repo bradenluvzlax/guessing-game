6fd36d3c2b69ce5ec52345014498c7145650bb2c
bradenluvzlax <18spd01@westottawa.net>
2017-05-02 10:06:29 -0400

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,13 +1,10 @@
 print ("welcome to the guessing game, You have five tries to guess the right number from 1-100")
 import random
-y = random.randint(1,100)
-print (y) 
+y = random.randint(1,100) 
 tries = 0
 while tries < 5:
     x = int(input("Guess a number 1-100: "))
-    if tries > 5:
-        print ("You lose!")
-    elif x < 1 or x > 100:
+    if x < 1 or x > 100:
         print ("Hey! that is not even between 1 and 100!")
         tries = tries + 1
     elif x == y:
@@ -15,12 +12,13 @@ while tries < 5:
     elif x < y:
         print ("Too low!")
         tries = tries + 1
-        print (5 - tries, "guesses left")
+        print (5 - tries, "guess(es) left")
     elif x > y:
         print ("Too high!")
         tries = tries + 1
-        print (5 - tries, "guesses left")
-
+        print (5 - tries, "guess(es) left")
+if tries > 5:
+    print ("You Lose!")
 
         
     

1b85c6b13dd8104b4f48d22781b5a1b4d40be119
bradenluvzlax <18spd01@westottawa.net>
2017-06-02 10:13:19 -0400

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,40 +1,46 @@
-print ("welcome to the guessing game, guess the randomly generated number in the range of 1-100 within 5 tries, and you win!")
-import random
-y = random.randint(1,100) 
-tries = 0
-while tries < 5:
-    x = int(input("Guess a whole number 1-100: "))
-    if x < 1 or x > 100:
-        print ("Hey! that is not even between 1 and 100!")
-        tries = tries + 1
-    elif x == y:
-        print ("nice guess!!! YOU WIN")
-        break
-    elif x < y:
-        tries = tries + 1
-        if tries == 5:
-            print ("You lose!")
-            print ("The number was", y)
+def gameloop():
+    print ("welcome to the guessing game, guess the randomly generated number in the range of 1-100 within 5 tries, and you win!")
+    import random
+    y = random.randint(1,100) 
+    tries = 0
+    while tries < 5:
+        x = int(input("Guess a whole number 1-100: "))
+        if x < 1 or x > 100:
+            print ("Hey! that is not even between 1 and 100!")
+            tries = tries + 1
+        elif x == y:
+            print ("nice guess!!! YOU WIN")
             break
-        print ("Too low!")
-        if tries == 4:
-            print (5 - tries, "guess left")
-        else:
-            print (5-tries, "guesses left")
-    elif x > y:
-        tries = tries + 1
-        if tries == 5:
-            print ("You Lose!")
-            print ("The number was", y)
-            break
-        print ("Too high!")
-        if tries == 4:
-            print (5 - tries, "guess left")
-        else:
-            print (5-tries, "guesses left")
-    elif tries == 5:
-        print ("You Lose!")
-        print ("The number was", y)
+        elif x < y:
+            tries = tries + 1
+            if tries == 5:
+                print ("You lose!")
+                print ("The number was", y)
+                z = input("Play again?Y/N: ")
+                if z == "Y":
+                    replay()
+                elif z == "N":
+                    print ("Thanks for playing!")
+                break
+            print ("Too low!")
+            if tries == 4:
+                print (5 - tries, "guess left")
+            else:
+                print (5-tries, "guesses left")
+        elif x > y:
+            tries = tries + 1
+            if tries == 5:
+                print ("You Lose!")
+                print ("The number was", y)
+                replay()
+            print ("Too high!")
+            if tries == 4:
+                print (5 - tries, "guess left")
+            else:
+                print (5-tries, "guesses left")
+def replay():
+    gameloop()
+gameloop()
         
     
     
